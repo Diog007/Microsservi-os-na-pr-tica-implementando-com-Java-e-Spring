2fd5b738eb6ccb8b953535fbbff888f8f6647f28
Diog007 <diogo.mz123@gmail.com>
2024-07-23 09:49:05 -0300

    Class-Adjusted DTO

--- src/main/java/com/alurafood/pagamentos/dto/PagamentoDto.java
+++ src/main/java/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -1,18 +1,48 @@
 package com.alurafood.pagamentos.dto;
 
 import com.alurafood.pagamentos.model.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
-public record PagamentoDto(
-        Long id,
-        BigDecimal valor,
-        String nome,
-        String numero,
-        String expiracao,
-        String codigo,
-        Status status,
-        Long pedidoId,
-        Long formaDePagamentoId
-) {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagamentoDto {
+
+    private Long id;
+
+    @NotNull
+    @Positive
+    private BigDecimal valor;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 19)
+    private String numero;
+
+    @NotBlank
+    @Size(max = 7)
+    private String expiracao;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String codigo;
+
+    private Status status;
+
+    @NotNull
+    private Long pedidoId;
+
+    @NotNull
+    private Long formaDePagamentoId;
 }
